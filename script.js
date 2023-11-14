ee2784ed113ed506f1113a81fb53d7fdb93599a0
Sujanshrestha <69748127+Sujan-shrestha7@users.noreply.github.com>
2023-11-15 04:08:30 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 
+const btnPopup = document.querySelector('.button');
+const loginClose = document.querySelector('.icon-close');
+
 registerLink.addEventListener('click',()=>{
-wrapper.classList.add('active');
-} );
+    wrapper.classList.add('active');
+    } );
 
 loginLink.addEventListener('click',()=>{
-    wrapper.classList.remove('active');
+wrapper.classList.remove('active');
     } );
+btnPopup.addEventListener('click',()=>{
+    wrapper.classList.add('active-popup');
+    } );
+
+loginClose.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup');
+    } );        
